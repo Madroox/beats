55e153d88efe5a0259ce8c7495e1256537ea6c04
Madroox <92368724+Madroox@users.noreply.github.com>
2021-12-18 10:32:10 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -33,6 +33,39 @@ hideMenuClose.addEventListener("click", function (event) {
     }
 })
 
+//Всплывающее меню Команда
+let openItem = item => {
+    let conteiner = item.closest(".team__item");
+    let contentBlock = conteiner.find(".team__hidden-conteiner");
+    let textBlock = contentBlock.find(".team__hidden-conteiner__block");
+    let reqHeight = textBlock.height();
+
+
+    conteiner.addClass("active");
+    contentBlock.height(reqHeight);
+}
+
+let closeItem = conteiner => {
+    let item = conteiner.find(".team__hidden-conteiner");
+    let itemConteiner = conteiner.find(".team__item");
+
+    itemConteiner.removeClass("active");
+    item.height(0);
+}
+
+$(".team__link").click(e => {
+    let $this = $(e.currentTarget);
+    let conteiner = $this.closest(".team__list");
+    let elemConteiner = $this.closest(".team__item");
+
+    if (elemConteiner.hasClass("active")) {
+        closeItem(conteiner);
+    } else {
+        closeItem(conteiner);
+        openItem($this);
+    }
+})
+
 // Отзывы
 let findBlock = (block) => {
     return $(".rewiews__person").filter((index, item) => {
@@ -150,30 +183,100 @@ $(".js-button").on("click", e => {
     $.fancybox.close();
 })
 
+//Функция "Смена цвета Фиксировонного меню"
+let changeMenuColorSection = sectionEq => {
+    let currentSection = section.eq(sectionEq);
+    let sectionColor = currentSection.attr("data-color-theme");
+    let activeClass = "fixed-menu--color";
 
-// Scrol
-
-//Переменные
-let section = $(".section");
-let wrapper = $(".wrapper__conteiner");
-let sideMenu = $(".fixed-menu");
-let fixedMenu = sideMenu.find(".fixed-menu__element");
-
-let mobileDetect = new MobileDetect(window.navigator.userAgent);
-let isMobile = mobileDetect.mobile();
-
-let inScroll = false;
-
-// Функция "Смена Позиции"
-let countSectionPosition = sectionEq => {
-    let position = sectionEq * -100;
-
-    if (isNaN(position)) {
-        console.error("Передано не верное значение countSectionPosition")
-        return 0;
+    if (sectionColor == "white") {
+        sideMenu.addClass("activeClass");
+    } else {
+        sideMenu.removeClass("activeClass");
     }
-    return position;
 }
+
+//Функция "Сброс Класса"
+let resetActiveClassForItem = (items, itemEq, activeClass) => {
+    items.eq(itemEq).addClass(activeClass).siblings().removeClass(activeClass);
+}
+
+section.first().addClass("active");
+
+let performTransition = (sectionEq) => {
+    if (inScroll) return;
+    let transitionOver = 1000;
+    let mouseInertiaOver = 300;
+    inScroll = true;
+
+    let position = countSectionPosition(sectionEq);
+
+    changeMenuColorSection(sectionEq);
+
+    wrapper.css({
+        transform: `translateY(${position}%)`
+    });
+
+    resetActiveClassForItem(section, sectionEq, "active");
+
+
+
+    setTimeout(() => {
+        inScroll = false;
+        resetActiveClassForItem(fixedMenu, sectionEq, "fixed-menu__element--active");
+    }, transitionOver + mouseInertiaOver)
+
+}
+
+let viewportScroller = () => {
+    let activeSection = section.filter(".active");
+    let nextSection = activeSection.next();
+    let prevSection = activeSection.prev();
+
+    return {
+        next() {
+            if (nextSection.length) {
+                performTransition(nextSection.index());
+            }
+        },
+        prev() {
+            if (prevSection.length) {
+                performTransition(prevSection.index());
+            }
+        }
+    }
+}
+
+$(window).on("wheel", e => {
+    let deltaY = e.originalEvent.deltaY;
+    let scroller = viewportScroller()
+
+    if (deltaY > 0) {
+        scroller.next();
+    }
+    if (deltaY < 0) {
+        scroller.prev();
+    }
+})
+
+$(window).on("keydown", e => {
+    let tagName = e.target.tagName.toLowerCase();
+    let userTypingInInputs = tagName === "input" || tagName === "textarea";
+    let scroller = viewportScroller()
+
+    if (userTypingInInputs) return;
+
+    switch (e.keyCode) {
+        case 38:
+            scroller.prev();
+            break;
+
+        case 40:
+            scroller.next();
+            break;
+    }
+
+})
 
 // Плавность перехода секций
 $(".wrapper").on("touchmove", e => {
@@ -189,3 +292,21 @@ $("[data-scroll-to]").click(e => {
 
     performTransition(reqSection.index());
 })
+
+//Свайп
+if (isMobile) {
+    $("body").swipe({
+        swipe: function (event, direction) {
+            let scroller = viewportScroller();
+            let scrollDirection = "";
+
+            if (direction == "up") scrollDirection = "next";
+            if (direction == "down") scrollDirection = "prev";
+
+            if (scrollDirection.length !== 0) {
+                scroller[scrollDirection]();
+            }
+
+        }
+    });
+}
